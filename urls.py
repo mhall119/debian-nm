995a0158e715c7cf20128e5a45711b913ca47355
Enrico Zini <enrico@enricozini.org>
2012-02-26 15:38:28 +0100

    Homepage, license, robots.txt

--- urls.py
+++ urls.py
@@ -1,13 +1,31 @@
+# NM website
+#
+# Copyright (C) 2012  Enrico Zini <enrico@debian.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import direct_to_template, redirect_to
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'nm.views.home', name='home'),
-    # url(r'^nm/', include('nm.foo.urls')),
+    url(r'^robots.txt$', direct_to_template, {'template': 'robots.txt', "mimetype": "text/plain"}, name="root_robots_txt"),
+    url(r'^$', direct_to_template, {'template': 'index.html'}, name="home"),
+    url(r'^license/$', direct_to_template, {'template': 'license.html'}, name="root_license"),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
