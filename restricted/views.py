f8da2350c99bc679bd23b1a82b422e247119c40c
Enrico Zini <enrico@enricozini.org>
2012-03-04 18:27:08 +0100

    Done with the DAM part of ammain

--- restricted/views.py
+++ restricted/views.py
@@ -95,6 +95,10 @@ def ammain(request):
     prog_dam_ok = bmodels.Process.objects.filter(progress=const.PROGRESS_DAM_OK) \
                  .annotate(started=Min("log__logdate")).order_by("started")
 
+    prog_dam_hold = bmodels.Process.objects.filter(progress=const.PROGRESS_DAM_HOLD) \
+                   .exclude(manager=None) \
+                   .annotate(started=Min("log__logdate")).order_by("started")
+
     return render_to_response("restricted/ammain.html",
                               dict(
                                   person=person,
@@ -107,6 +111,7 @@ def ammain(request):
                                   prog_fd_hold=prog_fd_hold,
                                   prog_fd_ok=prog_fd_ok,
                                   prog_dam_ok=prog_dam_ok,
+                                  prog_dam_hold=prog_dam_hold,
                               ),
                               context_instance=template.RequestContext(request))
 
