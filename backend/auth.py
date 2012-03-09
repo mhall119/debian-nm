aaf3a9594447e39c035f4c3bff60dd8233e3ef8e
Enrico Zini <enrico@enricozini.org>
2012-03-09 17:53:35 +0100

    Force logout if no user is present

--- backend/auth.py
+++ backend/auth.py
@@ -13,6 +13,43 @@ class FakeRemoteUser(object):
 class DACSRemoteUserMiddleware(django.contrib.auth.middleware.RemoteUserMiddleware):
     header = 'DACS_USERNAME'
 
+    def process_request(self, request):
+        from django.contrib import auth
+        from django.core.exceptions import ImproperlyConfigured
+
+        # AuthenticationMiddleware is required so that request.user exists.
+        if not hasattr(request, 'user'):
+            raise ImproperlyConfigured(
+                "The Django remote user auth middleware requires the"
+                " authentication middleware to be installed.  Edit your"
+                " MIDDLEWARE_CLASSES setting to insert"
+                " 'django.contrib.auth.middleware.AuthenticationMiddleware'"
+                " before the RemoteUserMiddleware class.")
+        try:
+            username = request.META[self.header]
+        except KeyError:
+            # If specified header doesn't exist then return (leaving
+            # request.user set to AnonymousUser by the
+            # AuthenticationMiddleware).
+
+            # Actually, make really sure we are logged out!
+            auth.logout()
+            return
+        # If the user is already authenticated and that user is the user we are
+        # getting passed in the headers, then the correct user is already
+        # persisted in the session and we don't need to continue.
+        if request.user.is_authenticated():
+            if request.user.username == self.clean_username(username, request):
+                return
+        # We are seeing this user for the first time in this session, attempt
+        # to authenticate the user.
+        user = auth.authenticate(remote_user=username)
+        if user:
+            # User is valid.  Set request.user and persist user in the session
+            # by logging the user in.
+            request.user = user
+            auth.login(request, user)
+
 class NMUserBackend(django.contrib.auth.backends.RemoteUserBackend):
     """
     RemoteUserBackend customised to create User objects from Person
