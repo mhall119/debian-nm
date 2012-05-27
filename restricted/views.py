d4ef4d0f175c2bf50980422b9bf51b8b803c679a
Enrico Zini <enrico@enricozini.org>
2012-05-27 11:10:42 +0200

    Login that redirects to itself after auth

--- restricted/views.py
+++ restricted/views.py
@@ -370,3 +370,9 @@ def minechangelogs(request, key=None):
                               ),
                               context_instance=template.RequestContext(request))
 
+def login_redirect(request):
+    url = request.GET.get("url", None)
+    if url is None:
+        return redirect('home')
+    else:
+        return redirect(url)
