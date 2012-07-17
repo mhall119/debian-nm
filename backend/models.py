e6a1f1a07f5d19756e5b726f622dfd845392cc41
Enrico Zini <enrico@enricozini.org>
2012-07-17 22:42:44 +0200

    After testing

--- backend/models.py
+++ backend/models.py
@@ -444,7 +444,7 @@ class Process(models.Model):
     archive_key = models.CharField("mailbox archive key", max_length=128, null=False, unique=True)
 
     def save(self, *args, **kw):
-        if self.archive_key is None:
+        if not self.archive_key:
             ts = datetime.datetime.utcnow().strftime("%Y%m%d%H%M%S")
             if self.person.uid:
                 self.archive_key = "%s-%s-%s" % (ts, self.applying_for, self.person.uid)
