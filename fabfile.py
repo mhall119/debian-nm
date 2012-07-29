d75eff1a75800e6ff2c3d4ea04ad0371df891c13
Enrico Zini <enrico@enricozini.org>
2012-07-29 20:37:41 +0200

    Added fabfile

---
+++ fabfile.py
@@ -0,0 +1,19 @@
+# file:///usr/share/doc/fabric/html/tutorial.html
+
+from fabric.api import local, run, sudo, cd, env
+
+env.hosts = ["nono.debian.org"]
+
+def prepare_deploy():
+    #local("./manage.py test my_app")
+    #local("git add -p && git commit")
+    local("test `git ls-files -cdmu | wc -l` = 0")
+    local("git push")
+
+def deploy():
+    prepare_deploy()
+    deploy_dir = "/srv/nm.debian.org/nm2"
+    with cd(deploy_dir):
+        sudo("git pull", user="nm")
+        sudo("./install_static", user="nm")
+        sudo("touch nm2.wsgi", user="nm")
