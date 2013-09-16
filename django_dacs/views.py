4bf5e6a945dc1ed2d05e44d59844121f26e11ed8
Enrico Zini <enrico@enricozini.org>
2013-09-16 12:01:52 +0200

    More generic redirect for missing login urls

--- django_dacs/views.py
+++ django_dacs/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import redirect
 
 def login_redirect(request):
     url = request.GET.get("url", None)
-    if url is None:
-        return redirect('home')
+    if not url:
+        return redirect('/')
     else:
         return redirect(url)
 
