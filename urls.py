18c55ff72c6a257215c8abee48142bb5fc0de893
Marco Bardelli <bardelli.marco@gmail.com>
2014-01-08 03:07:05 +0100

    Localisation bootstrapped only for (de,es,fr,it) using html5 modal

--- urls.py
+++ urls.py
@@ -25,6 +25,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^robots.txt$', TemplateView.as_view(template_name='robots.txt', content_type="text/plain"), name="root_robots_txt"),
+    url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^$', TemplateView.as_view(template_name='index.html'), name="home"),
     url(r'^license/$', TemplateView.as_view(template_name='license.html'), name="root_license"),
     url(r'^faq/$', TemplateView.as_view(template_name='faq.html'), name="root_faq"),
