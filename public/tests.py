08c2ce97efdba8f5faa7b85871afd224c548a8a1
Michael Hall <mhall119@ubuntu.com>
2014-02-01 23:44:00 -0500

    Start adding unit tests

--- public/tests.py
+++ public/tests.py
@@ -6,11 +6,30 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from model_mommy import mommy as factory
 
+from backend.models import Person, Process
+from backend import const
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+class PersonPageTestCase(TestCase):
+    
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+        
+    def test_person_listing(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Tests that a person is listed on the public persons page
         """
-        self.assertEqual(1 + 1, 2)
+        
+        # Create a test person and process
+        person = factory.make_one(Person, cn='Test', sn='User', email='testuser@debian.org')
+        process = factory.make_one(Process, person=person, applying_as=const.ALL_STATUS_BYTAG['mm'], applying_for=const.ALL_STATUS_BYTAG['dm'])
+        
+        # Check that the new person is listed on the page
+        response = self.client.get('/public/people')
+        self.assertContains(response, 'Test User', 1)
+        
+        self.assertContains(response, 'testuser@debian.org', 2)
